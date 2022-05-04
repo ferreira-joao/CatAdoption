236df925a8f434a7f473e7875f459b2d70a9e243
ferreira-joao <klausblack231@gmail.com>
2022-05-04 11:52:33 -0300

    initial config

---
+++ src/global/theme.tsx
@@ -0,0 +1,26 @@
+const size = {
+  xxs: "320px",
+  xs: "375px",
+  sm: "425px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1440px",
+  xxl: "2560px",
+};
+
+export const breakpoint = {
+  xxs: `(min-widht: ${size.xxs})`,
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`,
+};
+
+export const colors = {
+  primary: "#ff3300",
+  secondary: "#ff5c33",
+  tertiary: "#ff8566",
+  text: "#999999",
+};
